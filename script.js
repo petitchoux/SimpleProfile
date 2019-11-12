01ea692c20e0da2cad8d6a05277cd4a7c1304e1a
Joyce <joycalation@gmail.com>
2019-11-12 14:12:20 -0500

    Removed jQuery

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-$(function () {
-    $(document).scroll(function () {
-        var $nav = $("#mainNavbar");
-        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-    });
-});
+const nav = document.querySelector('#mainNavbar');
+const navTop = nav.offsetTop;
+
+function stickyNavigation() {
+    if (window.scrollY >= navTop) {
+        document.body.style.paddingTop = nav.offsetHeight + 'px';
+        document.body.classList.add('fixed-nav');
+    } else {
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove('fixed-nav');
+    }
+}
+
+window.addEventListener('scroll'. stickyNavigation);
